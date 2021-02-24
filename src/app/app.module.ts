ebb0df6af6975a77b3dc7348eb3cf302af7ef6be
WissemAouichi <wissem.aouichi@gmail.com>
2021-02-24 15:29:59 +0100

    Added routing to the components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,21 @@ import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
 import { EventService } from './events/shared/event.service'
 import { ToastrService } from './common/toastr.service';
+import { EventDetailsComponent } from './events/event-details/event-details.component';
+import { appRoutes } from './routes'
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   declarations: [
     EventsAppComponent,
     EventsListComponent,
     EventThumbnailComponent,
-    NavBarComponent
+    NavBarComponent,
+    EventDetailsComponent
   ],
   providers: [
     EventService,
--- src/app/events/shared/event.service.ts
+++ src/app/events/shared/event.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core'
 
+
 @Injectable()
 
 export class EventService {
     getEvents () {
         return EVENTS
+    }
+    getEvent(id:number){
+      return EVENTS.find(event=>event.id===id)
     }
 }
 
