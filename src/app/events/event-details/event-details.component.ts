74034f3cc61994d14de37801b456cf850dd05509
WissemAouichi <wissem.aouichi@gmail.com>
2021-02-24 15:39:15 +0100

    added ActivatedRoute service to bind with route param

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,5 +1,7 @@
 import { Component } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 import { EventService } from '../shared/event.service'
+
 
 @Component({
 templateUrl:'./event-details.component.html',
@@ -11,9 +13,9 @@ styles:[`
 export class EventDetailsComponent {
 
     event:any
-    constructor(private eventService: EventService){}
+    constructor(private eventService: EventService, private route:ActivatedRoute){}
 
     ngOnInit(){
-        this.event=this.eventService.getEvent(1)
+        this.event=this.eventService.getEvent(+this.route.snapshot.params['id'])
     }
 }
