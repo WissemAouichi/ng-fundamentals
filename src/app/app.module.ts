3960d1e2d28765f9dedee4edf3f2c17db681f941
WissemAouichi <wissem.aouichi@gmail.com>
2021-03-02 22:43:29 +0100

    missing () that stopped all the work has been resolved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
