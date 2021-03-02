7f9efe1b1b6321bfdbdfac4d03e9a1bd3fe6947e
WissemAouichi <wissem.aouichi@gmail.com>
2021-03-02 14:23:32 +0100

    Added the router to the error component

--- src/app/events/event-details/event-route-activator.service.ts
+++ src/app/events/event-details/event-route-activator.service.ts
@@ -5,14 +5,15 @@ import { EventService } from '../shared/event.service'
 @Injectable()
 
 export class EventRouteActivator implements CanActivate{
-    constructor(private eventService:EventService private router:Router){
+    constructor(private eventService:EventService, private router:Router){
 
     }
 
     canActivate(route:ActivatedRouteSnapshot){
-       const eventExists = !!this.eventService.getEvent(route.params['id'])
+       const eventExists = !!this.eventService.getEvent(+route.params['id'])
     
         if (!eventExists)
         this.router.navigate(['/404'])
+        return eventExists
     }
 }
