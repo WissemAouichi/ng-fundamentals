97775ed108428b5f2730c6b1e25ade3b0cd90a96
WissemAouichi <wissem.aouichi@gmail.com>
2021-03-21 22:14:32 +0100

    Auth provider added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { CreatEventComponent,
          EventsListComponent, 
          EventThumbnailComponent
  } from './events/index'
+import { AuthService } from './user/auth.service';
 
 @NgModule({
   imports: [
@@ -34,6 +35,7 @@ import { CreatEventComponent,
     ToastrService,
     EventRouteActivator,
     EventListResolver,
+    AuthService,
     { provide:'canDeactivateCreateEvent', 
       useValue: CheckDirtyState}
     ],
