39e05cbf175621b73f61e7a8a27862adf67640e1
WissemAouichi <wissem.aouichi@gmail.com>
2021-04-01 00:18:59 +0100

    Validators and style added to profile component

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,29 +1,40 @@
 import { Component, OnInit } from '@angular/core'
-import { FormControl, FormGroup } from '@angular/forms'
+import { FormControl, FormGroup, Validators } from '@angular/forms'
 import { Router } from '@angular/router'
 import { AuthService } from './auth.service'
 
 @Component({
   templateUrl: './profile.component.html',
+  styles:[`
+  em{float: right; color:#E05C65; padding-left:10px}
+  .error input {background-color:#E3C3C5;}
+  .error ::-webkit-input-placeholder { color: #999 }
+  .error ::moz-placeholder { color:#999 }
+  .error :moz-placeholder { color:#999 }
+  .error :ms-input-placeholder { color:#999 }
+  `]
 })
 export class ProfileComponent implements OnInit {
 
   profileForm: FormGroup
-  constructor(private authService:AuthService, private router:Router){}
+  constructor(private authService: AuthService, private router: Router) { }
   ngOnInit() {
-    let firstName = new FormControl(this.authService.currentUser.firstName)
-    let lastName = new FormControl(this.authService.currentUser.lastName)
-    
-    this.profileForm= new FormGroup({
-      firstName:firstName,
-      lastName:lastName
+    let firstName = new FormControl(this.authService.currentUser.firstName, Validators.required)
+    let lastName = new FormControl(this.authService.currentUser.lastName, Validators.required)
+
+    this.profileForm = new FormGroup({
+      firstName: firstName,
+      lastName: lastName
     })
   }
 
-  saveProfile(formValues){
-this.authService.updateCurrentUser(formValues.firstName, formValues.lastName)
+  saveProfile(formValues) {
+    if (this.profileForm.valid) {
+      this.authService.updateCurrentUser(formValues.firstName, formValues.lastName)
+      this.router.navigate(['events'])
+    }
   }
-  cancel(){
+  cancel() {
     this.router.navigate(['events'])
   }
 }
