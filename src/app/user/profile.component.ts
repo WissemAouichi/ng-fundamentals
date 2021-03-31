c89ded03f2789f78d65218dd1855cf170a43d527
WissemAouichi <wissem.aouichi@gmail.com>
2021-04-01 00:34:50 +0100

    Validation changed

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -15,19 +15,26 @@ import { AuthService } from './auth.service'
   `]
 })
 export class ProfileComponent implements OnInit {
-
+  private firstName:FormControl
+  private lastName:FormControl
   profileForm: FormGroup
   constructor(private authService: AuthService, private router: Router) { }
   ngOnInit() {
-    let firstName = new FormControl(this.authService.currentUser.firstName, Validators.required)
-    let lastName = new FormControl(this.authService.currentUser.lastName, Validators.required)
+    this.firstName = new FormControl(this.authService.currentUser.firstName, Validators.required)
+    this.lastName = new FormControl(this.authService.currentUser.lastName, Validators.required)
 
     this.profileForm = new FormGroup({
-      firstName: firstName,
-      lastName: lastName
+      firstName: this.firstName,
+      lastName: this.lastName
     })
   }
 
+  validateFirstName(){
+   return this.firstName.valid || this.firstName.touched
+  }
+  validateLastName(){
+    return this.lastName.valid || this.lastName.touched
+   }
   saveProfile(formValues) {
     if (this.profileForm.valid) {
       this.authService.updateCurrentUser(formValues.firstName, formValues.lastName)
