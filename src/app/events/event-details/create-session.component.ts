5f84fc5de690f6775719e4091d42aa26e170d619
WissemAouichi <wissem.aouichi@gmail.com>
2021-07-10 23:57:45 +0100

    Formatted documents

--- src/app/events/event-details/create-session.component.ts
+++ src/app/events/event-details/create-session.component.ts
@@ -3,59 +3,76 @@ import { Form, FormControl, FormGroup, Validators } from "@angular/forms";
 import { ISession } from "../shared";
 
 @Component({
-    templateUrl: './create-session.component.html',
-    styles: [`
-        em{float: right; color:#E05C65; padding-left:10px}
-        .error input? .error select, .error textarea {background-color:#E3C3C5;}
-        .error ::-webkit-input-placeholder { color: #999 }
-        .error ::moz-placeholder { color:#999 }
-        .error :moz-placeholder { color:#999 }
-        .error :ms-input-placeholder { color:#999 }
-    `]
+  templateUrl: "./create-session.component.html",
+  styles: [
+    `
+      em {
+        float: right;
+        color: #e05c65;
+        padding-left: 10px;
+      }
+      .error input, .error select, .error textarea { background-color: #e3c3e5; } {
+        background-color: #e3c3c5;
+      }
+      .error ::-webkit-input-placeholder {
+        color: #999;
+      }
+      .error ::moz-placeholder {
+        color: #999;
+      }
+      .error :moz-placeholder {
+        color: #999;
+      }
+      .error :ms-input-placeholder {
+        color: #999;
+      }
+    `,
+  ],
 })
-
 export class CreateSessionComponent implements OnInit {
-    newSessionForm: FormGroup
-    name: FormControl
-    presenter: FormControl
-    duration: FormControl
-    level: FormControl
-    abstract: FormControl
+  newSessionForm: FormGroup;
+  name: FormControl;
+  presenter: FormControl;
+  duration: FormControl;
+  level: FormControl;
+  abstract: FormControl;
 
-    ngOnInit() {
-        this.name = new FormControl('', Validators.required)
-        this.presenter = new FormControl('', Validators.required)
-        this.duration = new FormControl('', Validators.required)
-        this.level = new FormControl('', Validators.required)
-        this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400)])
+  ngOnInit() {
+    this.name = new FormControl("", Validators.required);
+    this.presenter = new FormControl("", Validators.required);
+    this.duration = new FormControl("", Validators.required);
+    this.level = new FormControl("", Validators.required);
+    this.abstract = new FormControl("", [
+      Validators.required,
+      Validators.maxLength(400),
+      this.restrictedWord
+    ]);
 
-        this.newSessionForm = new FormGroup({
-            name: this.name,
-            presenter: this.presenter,
-            duration: this.duration,
-            level: this.level,
-            abstract: this.abstract
-        })
-        console.log(this.newSessionForm)
-    }
-private restrictedWord(control:FormControl):{[key: string]:any}{
-return control.value.includes('foo')
-? {'restrictedWords':'foo'}
-: null
-}
-    saveSession(formValue) {
-        let session: ISession = {
-            id: undefined,
-            name: formValue.name,
-            presenter: formValue.presenter,
-            duration: +formValue.duration,
-            level: formValue.level,
-            abstract: formValue.abstract,
-            voters: []
-        }
-        console.log(session)
-    }
-    onClick(){
-        console.log(this.newSessionForm)
-    }
+    this.newSessionForm = new FormGroup({
+      name: this.name,
+      presenter: this.presenter,
+      duration: this.duration,
+      level: this.level,
+      abstract: this.abstract,
+    });
+    console.log(this.newSessionForm);
+  }
+  private restrictedWord(control: FormControl): { [key: string]: any } {
+    return control.value.includes('foo') ? { 'restrictedWords': 'foo' } : null;
+  }
+  saveSession(formValue) {
+    let session: ISession = {
+      id: undefined,
+      name: formValue.name,
+      presenter: formValue.presenter,
+      duration: +formValue.duration,
+      level: formValue.level,
+      abstract: formValue.abstract,
+      voters: [],
+    };
+    console.log(session);
+  }
+  onClick() {
+    console.log(this.newSessionForm);
+  }
 }
